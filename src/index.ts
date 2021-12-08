0251c75bb9b027e687e74f33d624836de69e2b13
fujia <fujia.site@outlook.com>
2021-12-08 19:01:14 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: fujia
+ * @Date: 2021-12-08 18:59:01
+ * @LastEditTime: 2021-12-08 19:00:25
+ * @LastEditors: fujia(as default)
+ * @Description: Alias for stage, so you can use npm init stage demos.
+ * @FilePath: /create-stage/src/index.ts
+ */
+
+const createStage = () => {
+  console.log('Hi! stage...');
+};
+
+export default createStage;
