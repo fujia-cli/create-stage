5f395b5506c835353a48d5b9c99b5e2a6e2b27dc
fujia <fujia.site@outlook.com>
2022-03-14 19:35:36 +0800

    fix: fixed run failed

--- bin/create-stage.js
+++ bin/create-stage.js
@@ -5,6 +5,7 @@
 const cp = require("child_process");
 const path = require("path");
 const { argv } = require("process");
+const { pathExistSync } = require("@fujia/check-path");
 
 const isWin32 = process.platform === "win32";
 const args = argv.slice(2);
@@ -17,20 +18,20 @@ const spawn = (command, args, options = {}) => {
   return cp.spawn(cmd, cmdArgs, options);
 };
 
-const child = spawn(
-  "node",
-  [path.resolve(__dirname, "../node_modules/@fujia/cli-core/bin/index.js"), "init", ...args],
-  {
+const execFilePath = path.resolve(__dirname, "../node_modules/@fujia/cli-core/bin/index.js");
+
+if (pathExistSync(execFilePath)) {
+  const child = spawn("node", [execFilePath, "init"], {
     cwd: process.cwd(),
     stdio: "inherit",
-  }
-);
+  });
 
-child.on("error", (err) => {
-  console.error(err);
-  process.exit(1);
-});
+  child.on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
-child.on("exit", (err) => {
-  process.exit(err);
-});
+  child.on("exit", (err) => {
+    process.exit(err);
+  });
+}
