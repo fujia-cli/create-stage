225be79cff0738bd77dc61365b6329695fbb635c
fujia <fujia.site@outlook.com>
2022-03-14 18:57:10 +0800

    feat: finished basic functionalities

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # create-stage
-Alias for stage, so you can use npm init stage demos.
+
+Alias for [stage](https://www.npmjs.com/package/@fujia/cli-core), so you can use `npm init stage demos`.
+
+Thanks to `npm init` feature introduced at npm@6, see [npm-init](https://docs.npmjs.com/cli/init) for more details.
+
+## Usage
+
+```sh
+
+npm init stage demos
+
+```
--- bin/create-stage.js
+++ bin/create-stage.js
@@ -2,20 +2,35 @@
 
 "use strict";
 
-// const cp = require("child_process");
-// const { argv } = require("process");
-// const stage = require("@fujia/cli-core");
+const cp = require("child_process");
+const path = require("path");
+const { argv } = require("process");
 
-// const isWin32 = process.platform === "win32";
-// const args = argv.slice(2);
+const isWin32 = process.platform === "win32";
+const args = argv.slice(2);
 
-// const spawn = (command, args, options = {}) => {
-//   const cmd = isWin32 ? "cmd" : command;
+const spawn = (command, args, options = {}) => {
+  const cmd = isWin32 ? "cmd" : command;
 
-//   const cmdArgs = isWin32 ? ["/c"].concat(command, args) : args;
+  const cmdArgs = isWin32 ? ["/c"].concat(command, args) : args;
 
-//   return cp.spawn(cmd, cmdArgs, options);
-// };
+  return cp.spawn(cmd, cmdArgs, options);
+};
 
-// spawn()
-require("@fujia/cli-core/bin");
+const child = spawn(
+  "node",
+  [path.resolve(__dirname, "../node_modules/@fujia/cli-core/bin/index.js"), "init", ...args],
+  {
+    cwd: process.cwd(),
+    stdio: "inherit",
+  }
+);
+
+child.on("error", (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+child.on("exit", (err) => {
+  process.exit(err);
+});
